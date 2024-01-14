871a68106521f913dac572bf31e3d6339959fac3
narayanipatil99 <patil.nar@northeastern.edu>
2024-01-13 23:44:59 -0500

    Dark theme: theme changed

--- src/portfolio.js
+++ src/portfolio.js
@@ -7,27 +7,26 @@ const settings = {
 
 //SEO Related settings
 const seo = {
-  title: "Ashutosh's Portfolio",
+  title: "Narayani's Portfolio",
   description:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   og: {
-    title: "Ashutosh Hathidara Portfolio",
+    title: "Narayani Patil Portfolio",
     type: "website",
-    url: "http://ashutoshhathidara.com/",
+    url: "http://narayanipatil.com/",
   },
 };
 
 //Home Page
 const greeting = {
-  title: "Ashutosh Hathidara",
-  logo_name: "AshutoshHathidara",
-  nickname: "layman_brother",
+  title: "Narayani Patil",
+  logo_name: "narayani_patil",
+  nickname: "Narayani",
   subTitle:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
-  resumeLink:
-    "https://drive.google.com/file/d/1FL_wydnPF2SWCdw2goYAgZ1KKBx8TpcD/view?usp=sharing",
-  portfolio_repository: "https://github.com/ashutosh1919/masterPortfolio",
-  githubProfile: "https://github.com/ashutosh1919",
+  resumeLink: "tbd",
+  portfolio_repository: "https://github.com/narayanipatil99",
+  githubProfile: "https://github.com/narayanipatil99",
 };
 
 const socialMediaLinks = [
